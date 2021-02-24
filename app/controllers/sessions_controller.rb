1838b59e5866947fe8231431ecede6eff0b01a08
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2021-02-23 20:47:56 -0800

    update controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,36 +1,50 @@
- class SessionsController < ApplicationController
+class SessionsController < ApplicationController
  
- get '/sessions/login' do 
-      if logged_in?
-        user_id = session[:user_id]
-        # puts @user.id.inspect
-        redirect to "users/#{user_id}"
-      else
-        erb:"sessions/login"
-      end
-    end
+ #sign up
 
-   
-    #log in/authenticate
-   
-    post '/login' do
-      # puts params.inspect
-      user = User.find_by(:email => params[:email])
-      if user && user.authenticate(params[:password])
-        session[:user_id] = user.id
-        redirect "users/#{user.id}"
-      else
-        redirect to '/users/new'
-      end
-    end
+  get'/signup' do
+    redirect '/home' if authorized?
+    erb :"/users/register"
+  end
+
+  #login, if logged in already, sends user back to home
   
-    get '/sessions/logout' do
-      if logged_in?
-        session.destroy
-        redirect to '/'
-      else
-        redirect to '/'
-      end
+  get '/login' do
+    redirect '/home' if authorized?
+    @failed = false
+    erb:'/users/login'
+  end
+
+  #post request for log in form
+
+  post '/login' do
+    user = User.find_by(username : params[:username])
+    if !!user && user.authenticate(params[:password])
+      session[:user_id = user.id]
+      redirect "/home"
+    else
+      @failed = true
+      erb:'/users/login'
     end
-    
+  end
+
+  post '/signup' do
+    redirect 'home' if authorized?
+    @user = User.new(params[:user])
+    if @user.save
+      session[:usr_id] = @user.id
+      redirect '/home'
+    else
+      erb : '/users/signup'
+    end
+  end
+
+  delete 'logout' do
+    sessions.clear if logged_in?
+    redirect '/'
+  end
+
+
+  
 end
+
