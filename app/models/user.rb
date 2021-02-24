3c5adc693fb43648d2b41f0676c7494f4818f052
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2021-02-23 22:12:50 -0800

    add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   has_many :keycaps
   has_many :switches
 
-  
+  validates :username, presence: true, uniqueness: true, length: { minimum: 2}, format: { with: /\A[a-zA-Z0-9_-]+\z/, message: "cannot contain blank spaces or symbols" }
+  validates :password_confirmation, presence: true, on: :create
+  validates :password, presence: true, length: { in: 6..20 }, confirmation: true, unless: ->(u){ u.password.blank? }
+
 
 end
